fcb65d7f80e1807a52a3c1f939b77a5dc09e0f88
ajit-jain <jainajit194@gmail.com>
2017-04-07 17:08:44 +0530

    changes

--- src/app/components/register-details/register-details.component.ts
+++ src/app/components/register-details/register-details.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-register-details',
   templateUrl: './register-details.component.html',
-  styles: []
+  styleUrls: ['../assets/css/style.css']
 })
 export class RegisterDetailsComponent implements OnInit {
 
